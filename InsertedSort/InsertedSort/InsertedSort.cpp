d9ca3025a85e304c75b57a9cfec74fda7f0678e8
BangSeIn <qkekehrtnfl97@gmail.com>
2020-02-10 20:55:22 +0900

    Basic Selected Sort (Inserted , Selected) algorithm in C++

---
+++ InsertedSort/InsertedSort/InsertedSort.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <limits>
+#include <iostream>
+#define SIZE 999
+using namespace std;
+
+int a[SIZE];
+
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main()
+{
+	int n, index;
+	cout << "Give the size of Array : "; cin >> n;
+	cout << "\nNow give values of each elements" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Element " << i + 1 << "st is : ";
+		cin >> a[i];
+		cout << endl;
+	}
+
+	//Inserted Sort
+	for (int i = 0; i <n-1;i ++) {
+		for (int j = i; j >= 0; j--) {
+			if (a[j] > a[j + 1]) swap(a[j], a[j + 1]);
+		}
+	}
+	
+	//Show sorted array
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << "  ";
+	}
+
+}
---
+++ SelectedSort/SelectedSort/SelectedSort.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <limits>
+#define SIZE 999
+using namespace std;
+
+int a[SIZE];
+
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main()
+{
+	int n, min, index;
+	cout << "Give the size of Array : "; cin >> n;
+	cout << "\nNow give values of each elements"<<endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Element " << i+1 << "st is : ";
+		cin >> a[i];
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++) {
+		min = INT_MAX;
+		for (int j = i; j < n; j++) {
+			if (min > a[j]) {
+				min = a[j];
+				index = j;
+			}
+		}
+		swap(a[i], a[index]);
+	}
+	cout << "Sorted : ";
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << "  ";
+	}
+	return 0;
+}
