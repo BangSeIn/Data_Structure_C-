7ca33fc6728104d684ee6985cf3c9b76a490aedf
BangSeIn <qkekehrtnfl97@gmail.com>
2020-02-20 23:12:27 +0900

    LSD_RadixSort in c++

--- Sort/Sort/Rsort.cpp
+++ Sort/Sort/Rsort.cpp
@@ -4,23 +4,25 @@ using namespace std;
 
 void RadixSort(Data* a, int n) {
 	Data res[SIZE], max = 0;
-	int exp = 1;
+	int digit = 1;
+	//Find the biggest data to set digit
 	for (int i = 0; i < n; i++) {
-		if (a[i] > max) max = a[i];
+		if (a[i] > max) max = a[i]; 
 	}
-	while (max / exp > 0) {
+
+	while (max / digit > 0) {
 		int bucket[10] = { 0 };
-		for (int i = 0; i < n; i++) bucket[a[i] / exp % 10]++;
+		for (int i = 0; i < n; i++) bucket[a[i] / digit % 10]++;
 		for (int i = 1; i < 10; i++) bucket[i] += bucket[i - 1];
 		for (int i = n - 1; i >= 0; i--) {
-			res[--(bucket[a[i] / exp % 10])] = a[i];
+			res[--(bucket[a[i] / digit % 10])] = a[i];
 			//res's (bucket[a[i] / exp %10]])th index must be (bucket[a[i]/exp%10]])-1
 
 		}
 		for (int i = 0; i < n; i++) a[i] = res[i];
 
 
-		exp *= 10;
+		digit *= 10;
 	}
 }
 
